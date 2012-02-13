21419b73fe7249487cba55995a8a4b0c00d47511
ChoJaewoo <chjw102@gmail.com>
2012-02-13 20:19:44 +0900

    Precompile Blueprint CSS in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ SampleApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  # Precompile Blueprint CSS
+  config.assets.precompile += ["blueprint/*.css"]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
