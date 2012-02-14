1b025c9ba7a35e1b9897ea6bd583c8c6d4860100
ChoJaewoo <chjw102@gmail.com>
2012-02-14 10:31:35 +0900

    Finish user signup

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
-    name "MyString"
-    email "MyString"
-  end
+	factory :user do
+		name "Kater102"
+		email "kater102@example.com"
+		password "foobar"
+	end
 end
